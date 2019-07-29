1372b74c23fe960e2e5677b975d03cdbecca8779
Neko3000 <Neko3000@live.com>
2019-07-29 16:14:27 +0900

    feat(UserCenterFavoriteViewController): finish layout for UserCenterFavoriteViewController

---
+++ Iris.iOS/ViewContollers/UserCenterFavoriateViewController.swift
@@ -0,0 +1,78 @@
+//
+//  UserCenterStatusViewController.swift
+//  Iris.iOS
+//
+//  Created by Xueliang Chen on 5/29/19.
+//  Copyright © 2019 Conceptual. All rights reserved.
+//
+
+import UIKit
+
+class UserCenterFavoriteViewController: UIViewController {
+
+    @IBOutlet weak var favoriteTableView: UITableView!
+    
+    @IBOutlet weak var favoriteTableViewHeightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        favoriteTableView.delegate = self
+        favoriteTableView.dataSource = self
+        favoriteTableView.register(UINib(nibName: "UserCenterFavoriteTableViewCell", bundle: nil), forCellReuseIdentifier: "UserCenterFavoriteTableViewCell")
+        
+        favoriteTableView.separatorStyle = .none
+        favoriteTableView.isScrollEnabled = false
+        
+        favoriteTableView.allowsSelection =  false
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        favoriteTableViewHeightConstraint.constant = 5 * ( 150 + 8 ) + 20
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UserCenterFavoriteViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell?
+        
+        let specificCell = tableView.dequeueReusableCell(withIdentifier: "UserCenterFavoriteTableViewCell") as! UserCenterFavoriteTableViewCell
+        
+        specificCell.setTableViewWidth(width:tableView.bounds.width)
+        
+        
+        specificCell.setContentImage(leftContentImage: UIImage(named: "user-center-favorite-ahri-1")!, rightContentImage: UIImage(named: "user-center-favorite-ahri-2")!, fullHeight: 142, fullWidth: tableView.frame.width, gapWidth: 8)
+        
+        cell = specificCell
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
---
+++ Iris.iOS/Views/UserCenterFavoriteTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  UserCenterStatusTableViewCell.swift
+//  Iris.iOS
+//
+//  Created by Xueliang Chen on 6/28/19.
+//  Copyright © 2019 Conceptual. All rights reserved.
+//
+
+import UIKit
+
+class UserCenterFavoriteTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var leftContentImageView: UIImageView!
+    
+    @IBOutlet weak var rightContentImageView: UIImageView!
+    
+    private var tableViewWidth:CGFloat = 0
+    
+    private var isInitialized:Bool = false
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if(!isInitialized){
+            
+            
+
+            isInitialized = true
+        }
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    public func setTableViewWidth(width:CGFloat){
+        tableViewWidth = width
+    }
+    
+    public func setContentImage(leftContentImage:UIImage,rightContentImage:UIImage,fullHeight:CGFloat,fullWidth:CGFloat,gapWidth:CGFloat){
+        
+        leftContentImageView.image = leftContentImage
+        rightContentImageView.image = rightContentImage
+        
+        leftContentImageView.frame = CGRect(x: 0, y: 0, width: leftContentImage.size.width * fullHeight / leftContentImage.size.height , height: fullHeight)
+        leftContentImageView.contentMode = .scaleToFill
+        
+        rightContentImageView.frame = CGRect(x: leftContentImageView.frame.width + gapWidth, y: 0, width: fullWidth - gapWidth - leftContentImageView.frame.width, height: fullHeight)
+        rightContentImageView.contentMode = .scaleAspectFill
+        
+    }
+}
