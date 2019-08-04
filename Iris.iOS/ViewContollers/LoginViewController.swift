7a88e375a109aa56f2062b726f76e7b33e73b782
Neko3000 <Neko3000@live.com>
2019-08-04 18:45:32 +0900

    feat(LoginViewController): change interface for ViewController because of OAuth2.0

--- Iris.iOS/ViewContollers/LoginViewController.swift
+++ Iris.iOS/ViewContollers/LoginViewController.swift
@@ -16,24 +16,11 @@ class LoginViewController: UIViewController{
     var loginWKWebView:WKWebView?
 
     @IBOutlet weak var loginBtn: UIButton!
-    @IBOutlet weak var signupBtn: UIButton!
-    
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var usernameImage: UIImageView!
-    @IBOutlet weak var passwordImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        usernameImage.layer.applySketchShadow(color: UIColor(named: "shadow-normal-purple")!, alpha: 0.3, x: 15, y: 10, blur: 6, spread: 0)
-        usernameTextField.attributedPlaceholder = NSAttributedString(string: "username", attributes: [.foregroundColor:UIColor(named: "input-normal-purple")!.withAlphaComponent(0.3)])
-        
-        passwordImage.layer.applySketchShadow(color: UIColor(named: "shadow-normal-purple")!, alpha: 0.3, x: 15, y: 10, blur: 6, spread: 0)
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [.foregroundColor:UIColor(named: "input-normal-purple")!.withAlphaComponent(0.3)])
-        
+
         loginBtn.layer.applySketchShadow(color: UIColor(named: "shadow-normal-purple")!, alpha: 0.3, x: 15, y: 20, blur: 30, spread: 0)
 
         
