5dcfbc02f989af26dd2816923d5818cfcd35ee09
Neko3000 <Neko3000@live.com>
2019-08-06 22:25:01 +0900

    feat(MainTabBarController): finish apperance for tabBar

--- Iris.iOS/MainTabBarController.swift
+++ Iris.iOS/MainTabBarController.swift
@@ -14,12 +14,28 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
+        tabBar.backgroundImage = UIImage(color: UIColor.white.withAlphaComponent(0.95))
+        tabBar.isTranslucent = true
         
+        tabBar.layer.borderWidth = 0
+        tabBar.clipsToBounds = true
+        
+        tabBar.items![0].image = UIImage(named: "icon-news")!.withRenderingMode(.alwaysOriginal)
+        tabBar.items![0].selectedImage = UIImage(named: "icon-news-active")!.withRenderingMode(.alwaysOriginal)
         tabBar.items![0].imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+
+        tabBar.items![1].image = UIImage(named: "icon-discover")!.withRenderingMode(.alwaysOriginal)
+        tabBar.items![1].selectedImage = UIImage(named: "icon-discover-active")!.withRenderingMode(.alwaysOriginal)
         tabBar.items![1].imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        
+        tabBar.items![2].image = UIImage(named: "icon-activity")!.withRenderingMode(.alwaysOriginal)
+        tabBar.items![2].selectedImage = UIImage(named: "icon-activity-active")!.withRenderingMode(.alwaysOriginal)
         tabBar.items![2].imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        
+        tabBar.items![3].image = UIImage(named: "icon-user-center")!.withRenderingMode(.alwaysOriginal)
+        tabBar.items![3].selectedImage = UIImage(named: "icon-user-center-active")!.withRenderingMode(.alwaysOriginal)
         tabBar.items![3].imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+
     }
     
     override func viewWillLayoutSubviews() {
---
+++ Iris.iOS/ViewContollers/ImageWithColorExtension.swift
@@ -0,0 +1,24 @@
+//
+//  ImageWithColorExtension.swift
+//  Iris.iOS
+//
+//  Created by Xueliang Chen on 8/6/19.
+//  Copyright © 2019 Conceptual. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
